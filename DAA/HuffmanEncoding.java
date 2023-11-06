1ad1907e7d25f0487df8c3ce5e4b0041e8f2b292
Asmita Chandanshive <78894711+asmita2953@users.noreply.github.com>
2023-11-06 06:06:01 +0530

    Update HuffmanEncoding.java

--- DAA/HuffmanEncoding.java
+++ DAA/HuffmanEncoding.java
@@ -1,74 +1,81 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
-import java.util.Comparator;
+import java.util.*;
 
-class HuffmanEncoding {
+class HuffmanNode implements Comparable<HuffmanNode> {
+    char data;
+    int frequency;
+    HuffmanNode left, right;
 
+    public HuffmanNode(char data, int frequency) {
+        this.data = data;
+        this.frequency = frequency;
+    }
 
-	public static void printCode(HuffmanNode root, String s)
-	{
-
-
-		if (root.left== null&& root.right== null && Character.isLetter(root.c)) {
-
-			System.out.println(root.c + ":" + s);
-			return;
-		}
-		printCode(root.left, s + "0");
-		printCode(root.right, s + "1");
-	}
-	public static void main(String[] args)
-	{
-
-		int n = 6;
-		char[] charArray = { 'a', 'b', 'c', 'd', 'e', 'f' };
-		int[] charfreq = { 5, 9, 12, 13, 16, 45 };
-		PriorityQueue<HuffmanNode> q
-			= new PriorityQueue<HuffmanNode>(n, new MyComparator());
-
-		for (int i = 0; i < n; i++) {
-
-			HuffmanNode hn = new HuffmanNode();
-
-			hn.c = charArray[i];
-			hn.data = charfreq[i];
-            hn.left = null;
-			hn.right = null;
-			q.add(hn);
-		}
-
-		HuffmanNode root = null;
-
-		while (q.size() > 1) {
-			HuffmanNode x = q.peek();
-			q.poll();
-			HuffmanNode y = q.peek();
-			q.poll();
-			HuffmanNode f = new HuffmanNode();
-			f.data = x.data + y.data;
-			f.c = '-';
-			f.left = x;
-			f.right = y;
-			root = f;
-			q.add(f);
-		}
-
-		printCode(root, "");
-	}
+    @Override
+    public int compareTo(HuffmanNode node) {
+        return this.frequency - node.frequency;
+    }
 }
 
-class HuffmanNode {
+public class Main {
 
-	int data;
-	char c;
-	HuffmanNode left;
-	HuffmanNode right;
-}
+    public static void main(String[] args) {
+        // Input from the user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String inputString = scanner.nextLine();
+        scanner.close();
 
+        // Generate Huffman codes
+        Map<Character, String> huffmanCodes = generateHuffmanCodes(inputString);
 
-class MyComparator implements Comparator<HuffmanNode> {
-	public int compare(HuffmanNode x, HuffmanNode y)
-	{
+        // Display Huffman codes
+        System.out.println("Huffman Codes:");
+        for (Map.Entry<Character, String> entry : huffmanCodes.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
+    }
 
-		return x.data - y.data;
-	}
+    public static Map<Character, String> generateHuffmanCodes(String inputString) {
+        // Count the frequency of each character in the input string
+        Map<Character, Integer> frequencyMap = new HashMap<>();
+        for (char c : inputString.toCharArray()) {
+            frequencyMap.put(c, frequencyMap.getOrDefault(c, 0) + 1);
+        }
+
+        // Create a priority queue of Huffman nodes
+        PriorityQueue<HuffmanNode> priorityQueue = new PriorityQueue<>();
+        for (Map.Entry<Character, Integer> entry : frequencyMap.entrySet()) {
+            priorityQueue.add(new HuffmanNode(entry.getKey(), entry.getValue()));
+        }
+
+        // Build the Huffman tree using a greedy strategy
+        while (priorityQueue.size() > 1) {
+            HuffmanNode left = priorityQueue.poll();
+            HuffmanNode right = priorityQueue.poll();
+            HuffmanNode newNode = new HuffmanNode('\0', left.frequency + right.frequency);
+            newNode.left = left;
+            newNode.right = right;
+            priorityQueue.add(newNode);
+        }
+
+        // Generate Huffman codes
+        HuffmanNode root = priorityQueue.poll();
+        Map<Character, String> huffmanCodes = new HashMap<>();
+        generateCodes(root, "", huffmanCodes);
+
+        return huffmanCodes;
+    }
+
+    private static void generateCodes(HuffmanNode node, String code, Map<Character, String> huffmanCodes) {
+        if (node != null) {
+            if (node.data != '\0') {
+                huffmanCodes.put(node.data, code);
+            }
+            generateCodes(node.left, code + "0", huffmanCodes);
+            generateCodes(node.right, code + "1", huffmanCodes);
+        }
+    }
 }
